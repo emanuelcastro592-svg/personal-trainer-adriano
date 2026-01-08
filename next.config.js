65468bcbde66d4e65bca7c5b47d2829d328351ac
emanuelcastro592-svg <emanuelcastro592@gmail.com>
2026-01-07 21:20:40 -0300

    Fix: configurar Next.js para build correto

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@prisma/client')
+    }
+    return config
+  },
+}
 
 module.exports = nextConfig
 
